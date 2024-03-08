b3c73317354c0b0be6f7e4b0ce16e42391c9e053
NitWitted <totallynitwitted@gmail.com>
2024-03-08 12:20:36 +0100

    missed two files in last commit

---
+++ apps/flutter-quest-api/Migrations/20240307102937_readd_isactive_bool_to_user.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlutterQuest.FlutterQuestApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class readd_isactive_bool_to_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+        }
+    }
+}
